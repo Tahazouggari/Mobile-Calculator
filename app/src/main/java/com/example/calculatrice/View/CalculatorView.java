d47a8a856100e54bf1cacc7c7165b0923ae2c921
Zouggari <tahazouggari61@gmail.com>
2024-11-19 22:30:19 +0100

    version0.0

--- app/src/main/java/com/example/calculatrice/View/CalculatorView.java
+++ app/src/main/java/com/example/calculatrice/View/CalculatorView.java
@@ -67,9 +67,9 @@ public class CalculatorView {
     private void setupControlButtons(AppCompatActivity activity) {
         activity.findViewById(R.id.buttonEquals).setOnClickListener(view -> viewModel.onEqualsClick());
         activity.findViewById(R.id.buttonClear).setOnClickListener(view -> viewModel.onClearClick());
-        activity.findViewById(R.id.PlusMins2).setOnClickListener(view -> viewModel.onPlusMins());
+        activity.findViewById(R.id.buttonPlusMinus).setOnClickListener(view -> viewModel.onPlusMins());
         activity.findViewById(R.id.buttonPercent).setOnClickListener(view -> viewModel.onPerCentClick());
-        activity.findViewById(R.id.buttondot).setOnClickListener(view -> viewModel.onDot());
+        activity.findViewById(R.id.buttonDot).setOnClickListener(view -> viewModel.onDot());
     }
 
 
